8733fcb7c8ce9424393674561676cc3ee7397167
Mark LaDoux <mark.ladoux@gmail.com>
2021-01-06 12:46:13 -0600

    update setup script

--- setup.sh
+++ setup.sh
@@ -10,9 +10,12 @@ source venv/bin/activate
 pip install pip --upgrade
 pip install $(cat requirements.txt)
 
-echo "Dependencies installed!"
-echo "Run \`source venv/bin/activate\` to enter virtual environment!"
-
+# Generate app secret
 SECRET_KEY=$(python -c 'from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())')
 
+# add app secret to environment config
 echo "SECRET_KEY=$SECRET_KEY" > app/.env
+
+# Notify user
+echo "Initial setup complete!"
+echo "Run \`source venv/bin/activate\` to enter virtual environment!"
